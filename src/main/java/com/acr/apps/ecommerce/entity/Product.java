4611991f9f7893ff457f98e312ff7360219d2e37
Rosy28 <rosycarmonauscanga@gmail.com>
2022-04-21 23:09:48 -0500

    entity pr

---
+++ src/main/java/com/acr/apps/ecommerce/entity/Product.java
@@ -0,0 +1,32 @@
+package com.acr.apps.ecommerce.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private Integer availableAmount;
+    private Integer Stock;
+    private Double qualification;
+    private String mark;
+    private String model;
+    private LocalDate year;
+    private LocalDateTime createdAt;
+}
