6bd70d3eab51bafc71bf4cd54a026514fd5247bf
CRISTHIAN VILLEGAS GARCIA <cristianvg9692@gmail.com>
2022-04-21 22:42:18 -0500

    Models

---
+++ src/main/java/com/acr/apps/ecommerce/entity/User.java
@@ -0,0 +1,30 @@
+package com.acr.apps.ecommerce.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String lastName;
+    private String email;
+    private String username;
+    private String password;
+    private String address;
+    private LocalDate birthday;
+    private LocalDateTime createdAt;
+}
