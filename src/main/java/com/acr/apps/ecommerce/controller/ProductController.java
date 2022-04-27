469cbb8f14f82f811a575fbdbe0616a44d331875
CRISTHIAN VILLEGAS GARCIA <cristianvg9692@gmail.com>
2022-04-26 23:21:29 -0500

    product controller updated

--- src/main/java/com/acr/apps/ecommerce/controller/ProductController.java
+++ src/main/java/com/acr/apps/ecommerce/controller/ProductController.java
@@ -1,14 +1,13 @@
 package com.acr.apps.ecommerce.controller;
 
+import com.acr.apps.ecommerce.dto.ProductDto;
+import com.acr.apps.ecommerce.dto.ResponseDto;
 import com.acr.apps.ecommerce.entity.Product;
 import com.acr.apps.ecommerce.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +22,12 @@ public class ProductController {
     public ResponseEntity<List<Product>> getAll(){
         List<Product> products = this.productService.getAll();
         return ResponseEntity.ok( products );
+    }
+
+    @PostMapping
+    public ResponseEntity<ResponseDto> save(@RequestBody ProductDto producto) {
+        ResponseDto response = new ResponseDto(null, "Registro exitoso...");
+        System.out.println(producto);
+        return new ResponseEntity( response,  HttpStatus.CREATED);
     }
 }
---
+++ src/main/java/com/acr/apps/ecommerce/dto/ResponseDto.java
@@ -0,0 +1,15 @@
+package com.acr.apps.ecommerce.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ResponseDto {
+    private Integer code;
+    private String message;
+}
